db83a6c8858ddb693172f0855630095593213d3f
Muradil Erkin <miller.muradil@gmail.com>
2024-05-08 20:09:51 -0400

    basic auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
   return (
     <main>
       <div>
-        <p>{session && <span>Logged in as {session.user?.name}</span>}</p>
+        <p>{session && <span>Logged in as {session.user.name}</span>}</p>
         <Link href={session ? "/api/auth/signout" : "/api/auth/signin"}>
           {session ? "Sign out" : "Sign in"}
         </Link>
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,12 +1,12 @@
-import { PrismaAdapter } from "@auth/prisma-adapter";
 import {
   getServerSession,
   type DefaultSession,
   type NextAuthOptions,
 } from "next-auth";
-import { type Adapter } from "next-auth/adapters";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
 
-import { env } from "~/env";
+// import { env } from "~/env";
 import { db } from "~/server/db";
 
 /**
@@ -37,25 +37,75 @@ declare module "next-auth" {
  */
 export const authOptions: NextAuthOptions = {
   callbacks: {
-    session: ({ session, user }) => ({
-      ...session,
-      user: {
-        ...session.user,
-        id: user.id,
+    async session({ session, token }) {
+      const user = {
+        id: token.id,
+        email: token.email,
+        username: token.username,
+        name: token.name,
+      };
+
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      session.user = user;
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token = {
+          token,
+          ...user,
+        };
+      }
+      return token;
+    },
+  },
+  session: {
+    strategy: "jwt",
+    maxAge: 24 * 60 * 60, // 1 day
+  },
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "Username" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, _req) {
+        if (!credentials?.username || !credentials?.password) {
+          return null;
+        }
+
+        const user = await db.account.findUnique({
+          where: {
+            username: credentials?.username,
+          },
+        });
+
+        if (!user) {
+          return null;
+        }
+
+        const isValid = await bcrypt.compare(
+          credentials.password,
+          user.password // hashed password saved in the db
+        );
+
+        // bcrypt.hash(credentials.password, 10, function (err, hash) {
+        //   console.log("==> hash", hash);
+        // });
+
+        if (!isValid) {
+          return null;
+        }
+
+        return {
+          id: user.id,
+          email: user.email,
+          username: user.username,
+          name: `${user.firstName} ${user.lastName}`,
+        };
       },
     }),
-  },
-  adapter: PrismaAdapter(db) as Adapter,
-  providers: [
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
   ],
 };
 
